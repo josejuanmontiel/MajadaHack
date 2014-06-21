b108d5a3541f1f4bb792078429fc50ef9c47536f
adrian sanchez diaz <adrians_diaz@hotmail.com>
2014-06-21 18:27:49 +0200

    no message

--- spring-boot-geo/src/main/webapp/js/js.js
+++ spring-boot-geo/src/main/webapp/js/js.js
@@ -21,13 +21,6 @@
     alert('error en el nick');
     error = error + 1;
   }
-  else if ( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(email))){
-	alert('error en el email');
-    error = error + 1;
-  }
- 
-  // Si el script ha llegado a este punto, todas las condiciones
-  // se han cumplido, por lo que se devuelve el valor true
   if (error == 0){
 	return true;
   }
